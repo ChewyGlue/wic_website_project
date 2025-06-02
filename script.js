206df77b1082d28f67508971ad9ed38c141e018e
ChewyGlue <97322833+ChewyGlue@users.noreply.github.com>
2025-06-01 22:55:24 -0700

    ass

--- script.js
+++ script.js
@@ -7,11 +7,14 @@
 // Step 1: Select the theme button
 
 let themeButton = document.getElementById("theme-button");
+let logo = document.getElementById("site-logo");
+
 // Step 2: Write the callback function
 const toggleDarkMode = () => {
-    // Write your code here
-    // This section will run whenever the button is clicked
-    document.body.classList.toggle("dark-mode");
+  // Write your code here
+  // This section will run whenever the button is clicked
+  document.body.classList.toggle("dark-mode");
+  logo.src = isDarkMode ? logo.dataset.dark : logo.dataset.light;
 }
 
 
@@ -27,3 +30,25 @@ themeButton.addEventListener("click", toggleDarkMode)
 /*** Form Validation [PLACEHOLDER] ***/
 /*** Animations [PLACEHOLDER] ***/
 /*** Success Modal [PLACEHOLDER] ***/
+
+
+
+/*** Form Handling ***
+  Purpose:
+  - When the user submits the form, the name and state they
+    entered should be added to the list of participants.
+***/
+
+
+// Step 1: Add your query for the submit RSVP button here
+
+
+const addParticipant = (event) => {
+  // Step 2: Write your code to manipulate the DOM here
+
+
+  event.preventDefault();
+}
+
+
+// Step 3: Add a click event listener to the submit RSVP button here
